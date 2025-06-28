60e62ccf906c1d21cf865858de1aaa88d5bf166e
BodartA <a.bodart23@gmail.com>
2025-06-28 13:39:22 +0200

    Initial Commit

---
+++ ToDoux/ToDouxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDouxApp.swift
+//  ToDoux
+//
+//  Created by Antoine BODART on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDouxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDouxTests/ToDouxTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDouxTests.swift
+//  ToDouxTests
+//
+//  Created by Antoine BODART on 28/06/2025.
+//
+
+import Testing
+@testable import ToDoux
+
+struct ToDouxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
