5a031cdbef0ad51bf2e507cb7d0cbe33ae53a41f
BodartA <a.bodart23@gmail.com>
2025-07-02 19:31:49 +0200

    Clean List display, ready to create new task and do task management

---
+++ ToDoux/ToDoList.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoList.swift
+//  ToDoux
+//
+//  Created by Antoine BODART on 02/07/2025.
+//
+
+import Foundation
+
+struct ToDoList: Identifiable {
+    
+    var id: UUID = UUID()
+    var name: String
+    var content: String
+    var isDone: Bool = false
+    
+    func createTask() {
+        
+    }
+    
+    func deleteTask() {
+        
+    }
+}
--- ToDoux/ToDoView.swift
+++ ToDoux/ToDoView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ToDoView: View {
-    var body: some View {
-            
+    
+    @State private var tasks: [ToDoList] = [
+        ToDoList(name: "Cleaning House", content: "Nothing To do", isDone: false),
+        ToDoList(name: "Sort pappers", content: "Nothing To do", isDone: true),
         
+    ]
+    
+    var body: some View {
+        
+            let pendingTasks = tasks.filter { !$0.isDone }
+            
             VStack(spacing: 10) {
                 
                 HStack() {
@@ -21,7 +29,7 @@ struct ToDoView: View {
                         .frame(width: 100, height: 100)
                     
                     VStack(spacing: 0) {
-                        Text("1 Task")
+                        Text("\(pendingTasks.count) \(pendingTasks.count > 1 ? "Tasks" : "Task")")
                             .font(.system(size: 25))
                             .opacity(0.5)
                             .fontWeight(.semibold)
@@ -42,16 +50,32 @@ struct ToDoView: View {
                 
                 // Liste
                 
+                ZStack(alignment: .bottomTrailing) {
+                    
+                    ScrollView() {
+                        VStack(alignment: .leading, spacing: 10) {
+                            ForEach(tasks) { task in
+                                
+                                HStack() {
+                                    /*@START_MENU_TOKEN@*/Text(task.name)/*@END_MENU_TOKEN@*/
+                                        .font(.headline)
+                                        .fontWeight(.bold)
+                                    Spacer()
+                                    Image(systemName: task.isDone ? "checkmark.circle.fill" : "circle")
+                                }
+                                .padding()
+                                .frame(width: .infinity, minHeight: 50)
+                                .background(task.isDone ? Color("CustomYellow").opacity(0.4) : Color("CustomYellow"))
+                                .foregroundColor(Color.black.opacity(0.8))
+                                .cornerRadius(15)
+                            }
+                        }
+                    }
+                    .padding(10)
+                    .zIndex(0)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                }
                 
-                Text("Liste ici")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .border(Color.green)
-                
-                // Bouton +
-                
-                Text("Bouton")
-                    .frame(maxWidth: .infinity, maxHeight: 50, alignment: .trailing)
-                    .border(Color.red)
             }
             .padding(24)
         }
